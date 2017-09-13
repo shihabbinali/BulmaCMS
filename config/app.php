c3403a6a83434fe46e2239d2a2eda41faa8bff31
Shihab Bin Ali <shihabbinali9@gmail.com>
2017-09-13 15:03:35 +0600

    Created new package for learning perpose, name is calculator

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Shihabbinali\Calculator\CalculatorServiceProvider::class,
 
         /*
          * Application Service Providers...
